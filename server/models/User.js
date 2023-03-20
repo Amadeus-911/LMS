6dd0b3517c87c3527dff47d9d7a25f6ba9f34419
BlackBird-911 <iftikharimrul@iut-dhaka.edu>
2023-03-20 12:38:20 +0600

    user model update

--- server/models/User.js
+++ server/models/User.js
@@ -26,6 +26,10 @@ const User = sequelize.define('User', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'student',
+    },
     createdAt: {
         type: DataTypes.DATE,
         allowNull: false,
